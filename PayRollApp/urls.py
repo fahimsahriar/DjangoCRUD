4720e1826a7da49dfdb40b32e06c5432e91306d6
Fahim Sahriar <shishirluth@gmail.com>
2024-10-02 21:22:29 +0600

    deleting an individual user

--- PayRollApp/urls.py
+++ PayRollApp/urls.py
@@ -7,4 +7,5 @@ from PayRollApp import views
 urlpatterns = [
     path('Employees/', views.EmployeesList, name='employe_list'),
     path('employee/<int:id>/', views.EmployeeDetails, name='employee_detail'),
+    path('DeleteEmployee/<int:id>/', views.EmployeeDelete, name='Delete_Employee'),
 ]
--- PayRollApp/views.py
+++ PayRollApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from PayRollApp.models import Employe
 
@@ -15,4 +15,35 @@ def EmployeeDetails(request, id):
     employee = Employe.objects.get(id = id)
     dict = {'employee' : employee}
     print(dict)
+    return render(request, TemplateFile, context=dict)
+
+#function for deleting an employee
+def EmployeeDelete(request, id):
+    TemplateFile = "PayRollApp/EmployeeDelete.html"
+    employee = Employe.objects.get(id = id)
+    dict = {'employee' : employee}
+    print(dict)
+
+    #delete button action
+    if(request.method == "POST"):
+        employee.delete()
+        return redirect("employe_list") #need to know how the arguments url works(relative url or static url)
+        #explanation of redirect function's first argument
+        # to: The destination to which you want to redirect the user. This can be:
+
+        # A URL pattern name (the name defined in urls.py).
+        #return redirect('employe_list')  # Assuming 'employe_list' is a URL name in urls.py
+
+        # A relative URL (e.g., "/persons/Employees").
+        #path('persons/Employees/', views.employe_list, name='employe_list')
+
+        # An absolute URL (e.g., "https://www.example.com").
+        # A model object with a get_absolute_url() method defined.
+
+        # *args: Positional arguments to pass if you're using a URL pattern name.
+        #return redirect('employee_detail', id=employee.id)
+
+        # **kwargs: Keyword arguments to pass if your URL pattern expects parameters (e.g., passing an id to a detail view).
+        #path('employees/<int:id>/', views.employee_detail, name='employee_detail')
+
     return render(request, TemplateFile, context=dict)
