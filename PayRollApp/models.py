fbf946e32683e7eecad33958ae9255126d1917f3
Fahim Sahriar <shishirluth@gmail.com>
2024-10-03 23:38:52 +0600

    redesigning models with foreign key, update query same as before

--- PayRollApp/models.py
+++ PayRollApp/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    CountryName = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.CountryName
+    
+class Department(models.Model):
+    DepartmentName = models.CharField(max_length=40)
+    LocationName = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.DepartmentName
+
 class Employe(models.Model):
     
     COUNTRIES =[
@@ -20,5 +33,7 @@ class Employe(models.Model):
     BirthDate = models.DateField()
     HiringDate = models.DateField()
     Notes = models.CharField(max_length=200)
-    Country = models.CharField(max_length=35, choices=COUNTRIES, default=None)
+    #Country = models.CharField(max_length=35, choices=COUNTRIES, default=None)
+    Country = models.ForeignKey("Country", default=0, on_delete=models.PROTECT)
     Email = models.EmailField(max_length=100, default="")
+    EmployeeDepartment = models.ForeignKey("Department", default=0, on_delete=models.PROTECT)
--- PayRollApp/views.py
+++ PayRollApp/views.py
@@ -5,7 +5,13 @@ from PayRollApp.models import Employe
 
 # Create your views here.
 def EmployeesList(request):
-    Employees = Employe.objects.all()
+    #Employees = Employe.objects.all()
+    Employees = Employe.objects.select_related('EmployeeDepartment', 'Country').all()
+    # for employee in Employees:
+    #     print(employee.FirstName, employee.LastName, employee.Salary)
+    #     fields = [field.name for field in employee._meta.get_fields()]
+    #     print(fields)
+
     TemplateFile = "PayRollApp/EmployeesList.html"
     dict = {'employees' : Employees}
     return render(request, TemplateFile, context=dict)
@@ -13,15 +19,16 @@ def EmployeesList(request):
 #function for individual employee detail
 def EmployeeDetails(request, id):
     TemplateFile = "PayRollApp/EmployeeDetails.html"
-    employee = Employe.objects.get(id = id)
-    dict = {'employee' : employee}
+    #employee = Employe.objects.get(id = id)
+    employee = Employe.objects.select_related('EmployeeDepartment', 'Country').all().filter(id = id)
+    dict = {'employee' : employee[0]}
     return render(request, TemplateFile, context=dict)
 
 #function for deleting an employee
 def EmployeeDelete(request, id):
     TemplateFile = "PayRollApp/EmployeeDelete.html"
-    employee = Employe.objects.get(id = id)
-    dict = {'employee' : employee}
+    employee = Employe.objects.select_related('EmployeeDepartment', 'Country').all().filter(id = id)
+    dict = {'employee' : employee[0]}
     print(dict)
 
     #delete button action
